d12b727860c73352032533da1cdcc679ee0e1c41
safi2k21 <91405246+safi2k21@users.noreply.github.com>
2021-09-26 11:09:43 +0500

    Add files via upload

---
+++ Stability of LTI Systems.m
@@ -0,0 +1,24 @@
+clear all
+close all
+clc
+n = 0:100;
+s1 = cos(2*pi*0.05*n); % A low frequency sinusoid
+s2 = cos(2*pi*0.47*n); % A high frequency sinusoid
+x = s1+s2;
+% Implementation of the moving average filter
+M = input('Desired length of the filter = ');
+num = ones(1,M);
+y = filter(num,1,x)/M;
+% Display the input and output signals
+subplot(2,2,1); plot(n,s1);
+axis([0, 100, -2, 2]);
+xlabel('Time index n'); ylabel('Amplitude'); title('Signal # 1');
+subplot(2,2,2); plot(n,s2);
+axis([0, 100, -2, 2]);
+xlabel('Time index n'); ylabel('Amplitude'); title('Signal # 2');
+subplot(2,2,3); plot(n,x);
+axis([0, 100, -2, 2]);
+xlabel('Time index n'); ylabel('Amplitude'); title('Input Signal');
+subplot(2,2,4); plot(n,y);
+axis([0, 100, -2, 2]); xlabel('Time index n'); ylabel('Amplitude');
+title('Output Signal');
